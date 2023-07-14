24e47fe304355fb59d7c738ff4f1d8532edcca85
EC2 Default User <ec2-user@ip-172-31-76-58.ec2.internal>
2023-07-14 16:20:06 +0000

    Initial commit

---
+++ test/efs_stack.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EfsStack from '../lib/efs_stack-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/efs_stack-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EfsStack.EfsStackStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
