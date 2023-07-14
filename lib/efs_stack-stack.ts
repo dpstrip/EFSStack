42e5c6e23ce71d6fb246eba8c949b149b1bb93cc
dpstrip <dpstrip@gmail.com>
2023-07-14 18:33:12 +0000

    Initial

--- lib/efs_stack-stack.ts
+++ lib/efs_stack-stack.ts
@@ -1,16 +1,35 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import * as efs from 'aws-cdk-lib/aws-efs';
 
 export class EfsStackStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'EfsStackQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    //find my exisiting vpc
+    const vpc = ec2.Vpc.fromLookup(this, 'ImportVPC', {vpcId: 'vpc-0b3e718e0d3a7b733'});
+  
+    
+    //create efs within the vpc
+    const efsSecurityGroup = new ec2.SecurityGroup(this, 'efs-sg',{
+      vpc,
+      allowAllOutbound: true,
+      description: 'security group for efs'
+    });
+    
+    const fileSytem = new efs.FileSystem(this, "MyEfsFileSystem",{
+      vpc,
+      encrypted: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      securityGroup: efsSecurityGroup,
+    });
+    
+    //create a volumn
+    const volume = { name: "volumn",
+      efsVolumnConfiguration: {
+        fileSystemId: fileSytem.fileSystemId
+      }
+  };
   }
 }
